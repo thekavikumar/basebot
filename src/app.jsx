6c3595426a0c79e8e36d6c3dcde304ff8f0c07a6
thekavikumar <kavikumarceo@gmail.com>
2023-06-13 11:45:02 +0530

    in

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,20 @@
-import Hero from "./components/hero.jsx";
-import Demo from "./components/demo.jsx";
+import Hero from "/components/hero.jsx";
+import Demo from "/components/demo.jsx";
 import "./app.css";
 
 const App = () => {
-    return (
-        <>
-            <div className='main'>
-                <div className='gradient' />
-            </div>
+  return (
+    <>
+      <div className="main">
+        <div className="gradient" />
+      </div>
 
-            <div className='app'>
-                <Hero />
-                <Demo />
-            </div>
-        </>
-    );
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </>
+  );
 };
 
 export default App;
